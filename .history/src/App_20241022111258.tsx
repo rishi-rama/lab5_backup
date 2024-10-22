0344108b64d75e020c654772359d0386bd61792b
rishi-rama <122561679+rishi-rama@users.noreply.github.com>
2024-10-22 11:29:55 -0700

    v1

---
+++ .history/src/App_20241022111258.tsx
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { MyBudgetTracker } from "./views/MyBudgetTracker";
+
+const App = () => {
+  // HINT: Wrap the MyBudgetTracker component with AppContextProvider
+  return <MyBudgetTracker />;
+};
+
+export default App;
--- .history/src/context/AppContext_20241022111258.tsx
+++ .history/src/context/AppContext_20241022111258.tsx
@@ -1,27 +1,28 @@
 import { createContext, useState } from "react";
-import { AppContextType, Expense } from "../types/types";
+import { Expense } from "../types/types";
 
-// Initialize with default values
+// Exercise: Create add budget to the context
+
+interface AppContextType {
+  expenses: Expense[];
+  setExpenses: React.Dispatch<React.SetStateAction<Expense[]>>;
+}
+
 const initialState: AppContextType = {
   expenses: [],
   setExpenses: () => {},
-  budget: 1000, // Default budget value
-  setBudget: () => {},
 };
 
 export const AppContext = createContext<AppContextType>(initialState);
 
 export const AppProvider = (props: any) => {
   const [expenses, setExpenses] = useState<Expense[]>(initialState.expenses);
-  const [budget, setBudget] = useState<number>(initialState.budget);
 
   return (
     <AppContext.Provider
       value={{
-        expenses,
-        setExpenses,
-        budget,
-        setBudget,
+        expenses: expenses,
+        setExpenses: setExpenses,
       }}
     >
       {props.children}
